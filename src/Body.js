682a8d6a907319572ec7b377c1703b2fbf8658d2
San <san.kuno22@gmail.com>
2023-02-21 13:14:35 +0100

    nothing changed

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function Body() {
-  return (
-    <div>
-      <img className={`background`} src="./img/turtle.jpg" />
-      <img className={`background`} src="./img/ship.jpg" />
-      <img className={`background`} src="./img/shark.jpg" />
-    </div>
-  );
+  return <div></div>;
 }
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,6 @@ export default function Header() {
   // const headerStyle = {background-image: url(./img/header.png) width="100%"}
   return (
     <div className={`header`}>
-      <img id="blackOcean" src="./img/header.png" width="100%"></img>
       <img id="logo" src="./img/logo.png" width="12%"></img>
       <button id="aboutButton">About Us</button>
       <button id="contact">Contact Us</button>
