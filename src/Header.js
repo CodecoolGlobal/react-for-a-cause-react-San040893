4cff89e852444888dd9031b19d7f5ccb25579b54
San <san.kuno22@gmail.com>
2023-02-21 09:43:03 +0100

    start of the header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className={`header`}>
+      <img
+        id="blackOcean"
+        src="./img/header.png"
+        width="100%"
+        height="40%"
+      ></img>
+      <img id="logo" src="./img/logo.png" width="12%"></img>
+    </div>
+  );
 }
