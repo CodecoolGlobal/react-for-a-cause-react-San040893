2485bce626ea0999c39f1768dfe43fd68f0e463e
San <san.kuno22@gmail.com>
2023-02-22 12:50:09 +0100

    hard reset

--- src/Landing.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import turtle from "./img/turtle.jpg";
-
-export default function Landing() {
-  return <div className="turtle"></div>;
-}
-// <img className={`background`} src={turtle} />;
